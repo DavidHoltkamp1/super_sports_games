4f19336ef56b6e6822fb086cede2adfdbaf51ec2
David Holtkamp <david.hholtkamp@gmail.com>
2019-11-21 15:25:29 -0700

    method for standard deviation

--- lib/event.rb
+++ lib/event.rb
@@ -1,4 +1,3 @@
-require_relative 'standard_deviation'
 
 class Event
 attr_reader :name, :ages
@@ -7,6 +6,7 @@ attr_reader :name, :ages
     @name = name
     @ages = ages
   end
+
   def max_age
     @ages.max
   end
@@ -19,7 +19,23 @@ attr_reader :name, :ages
     @ages.sum.to_f / @ages.length
   end
 
-  
+  def standard_deviation_age
+    subtract_by_average = []
+    ages.each do |age|
+      x = age - average_age
+      subtract_by_average << x.round(1)
+    end
+    squared_numbers = subtract_by_average.map do |number|
+      x = number ** 2
+      x.round(2)
+    end
+    sum_of_squared_numbers = squared_numbers.sum
+    divide = sum_of_squared_numbers / ages.length
+    square_root = Math.sqrt(divide).round(2)
+    square_root
+  end
+
+
 
 
 
