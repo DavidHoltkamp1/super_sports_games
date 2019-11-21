564ee59cc34e6857521ff32e6fb56686a2b0380f
David Holtkamp <david.hholtkamp@gmail.com>
2019-11-21 13:41:15 -0700

    subtract by average iteration and squared numbers

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,13 +1,29 @@
-ages = [24, 30, 18, 20, 41]
+class StandardDeviation
+  ages = [24, 30, 18, 20, 41]
 
-sum = ages.sum
-length = ages.length
+  sum = ages.sum
+  # length = ages.length
 
-average = sum.to_f / ages.length
+  average = (sum.to_f / ages.length).round(1)
 
-ages.map do |age|
-  
-require "pry"; binding.pry
+    subtract_by_average = []
+    ages.each do |age|
+      x = age - average
+      subtract_by_average << x.round(1)
+    end
+
+    squared_numbers = subtract_by_average.map do |number|
+      x = number ** 2
+      x.round(2)
+    end
+
+
+  p squared_numbers
+
+
+end
+
+
 # Your code here for calculating the standard deviation
 
 # When you find the standard deviation, print it out
