ed3556a60b79e21d7f4604928efc4171ad764a1c
David Holtkamp <david.hholtkamp@gmail.com>
2019-11-21 13:21:31 -0700

    Sum, length of array, and average

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,13 @@
 ages = [24, 30, 18, 20, 41]
 
+sum = ages.sum
+length = ages.length
+
+average = sum.to_f / ages.length
+
+ages.map do |age|
+  
+require "pry"; binding.pry
 # Your code here for calculating the standard deviation
 
 # When you find the standard deviation, print it out
